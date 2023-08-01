9f6ff1a64700d43dda06638cb3b41bff39c7b51f
chneau <charles63500@gmail.com>
2023-08-01 10:38:08 +0100

    WIP

--- bard.go
+++ bard.go
@@ -1,42 +1,55 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"os"
 	"regexp"
 	"strings"
 )
 
-type SNlM0e string
+type BardSession struct {
+	SessionId      string  `json:"session_id"`
+	SNlM0e         string  `json:"SNlM0e"`
+	ReqID          string  `json:"req_id"`
+	ConversationID *string `json:"conversation_id"`
+	ResponseID     *string `json:"response_id"`
+	ChoiceID       *string `json:"choice_id"`
+}
 
-func InitBard(sessionId string) (SNlM0e, error) {
+func InitBard(sessionId string) (*BardSession, error) {
 	request, err := http.NewRequest("GET", "https://bard.google.com", nil)
 	if err != nil {
-		return "", errors.New("failed to create request: " + err.Error())
+		return nil, errors.New("failed to create request: " + err.Error())
 	}
 	request.Header.Set("Cookie", "__Secure-1PSID="+sessionId)
 	response, err := http.DefaultClient.Do(request)
 	if err != nil {
-		return "", errors.New("failed to send request: " + err.Error())
+		return nil, errors.New("failed to send request: " + err.Error())
 	}
 	defer response.Body.Close()
 
 	// read body as string
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
-		return "", errors.New("failed to read response body: " + err.Error())
+		return nil, errors.New("failed to read response body: " + err.Error())
 	}
 
 	// regex match for SNlM0e /SNlM0e":"(.*?)"/
 	matches := regexp.MustCompile(`SNlM0e":"(.*?)"`).FindSubmatch(body)
 	if len(matches) < 2 {
-		return "", errors.New("failed to find SNlM0e in response body")
+		return nil, errors.New("failed to find SNlM0e in response body")
 	}
 
-	return SNlM0e(matches[1]), nil
+	return &BardSession{
+		SNlM0e:    string(matches[1]),
+		SessionId: sessionId,
+		ReqID:     "0",
+	}, nil
 }
 
 func ReadSavedSessionId() string {
@@ -86,4 +99,51 @@ func PromptSessionId() string {
 		panic("failed to write session ID: " + err.Error())
 	}
 	return sessionId
+}
+
+func (s BardSession) Ask(message string) (string, error) {
+	request, err := http.NewRequest("POST", "https://bard.google.com/_/BardChatUi/data/assistant.lamda.BardFrontendService/StreamGenerate", nil)
+	if err != nil {
+		return "", errors.New("failed to create request: " + err.Error())
+	}
+	urlQuery := request.URL.Query()
+	urlQuery.Add("bl", "boq_assistant-bard-web-server_20230730.19_p0")
+	urlQuery.Add("_reqID", s.ReqID)
+	urlQuery.Add("rt", "c")
+	request.URL.RawQuery = urlQuery.Encode()
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8")
+	request.Header.Set("Cookie", "__Secure-1PSID="+s.SessionId)
+
+	freqRight := []interface{}{[]string{message}, nil, []*string{s.ConversationID, s.ResponseID, s.ChoiceID}}
+	freqRightBytes, err := json.Marshal(freqRight)
+	if err != nil {
+		return "", errors.New("failed to marshal freq right: " + err.Error())
+	}
+	freqRightString := string(freqRightBytes)
+	freq := []interface{}{nil, freqRightString}
+	freqBytes, err := json.Marshal(freq)
+	if err != nil {
+		return "", errors.New("failed to marshal freq: " + err.Error())
+	}
+	freqString := string(freqBytes)
+	formData := url.QueryEscape("f.req") + "=" + url.QueryEscape(freqString) + "&" + url.QueryEscape("at") + "=" + url.QueryEscape(s.SNlM0e)
+	request.Body = io.NopCloser(strings.NewReader(formData))
+
+	response, err := http.DefaultClient.Do(request)
+	if err != nil {
+		return "", errors.New("failed to send request: " + err.Error())
+	}
+	defer response.Body.Close()
+
+	// read body as string
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return "", errors.New("failed to read response body: " + err.Error())
+	}
+	bodyStringParts := strings.Split(string(body), "\n")
+	if len(bodyStringParts) < 3 {
+		return "", errors.New("failed to parse response body")
+	}
+
+	return bodyStringParts[3], nil
 }
--- main.go
+++ main.go
@@ -9,4 +9,10 @@ func main() {
 		panic("failed to initialize bard: " + err.Error())
 	}
 	log.Println("bardCli: ", bardCli)
+
+	str, err := bardCli.Ask("What time is it?")
+	if err != nil {
+		panic("failed to ask question: " + err.Error())
+	}
+	log.Println("str: ", str)
 }
